f60dd1c47a3878399cf2ae3b56a40f4cfedec7b7
Krotera <01101011@tuta.io>
2020-09-15 17:34:51 -0400

    Add txt map editing and generation and add . floors

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
-#include <libtcod.hpp>
+// std
 #include <map>
 #include <tuple>
 #include <iostream>
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+
+// foreign
+#include <libtcod.hpp>
+
+// local
+// #include "map_or_something.cpp"
 
 typedef std::tuple<int, int> Coordinates;
 
@@ -142,58 +151,187 @@ class Map {
         }
 };
 
-int main() {
+/*
+ *  Renders a test Map, returning the Map and the player Coordinates
+ *  
+ *  @param none
+ *  @return std::tuple<Map, Coordinates>
+ */
+std::tuple<Map, Coordinates> renderTestMap() {
+    Object * wall  = new Object('#', "rock wall");
+    Object * floor = new Object('.', "wall floor");
+    Object * stick = new Object('/', "stick");
     int player_x = 3;
     int player_y = 4;
-    
-    Object * rock  = new Object('#', "rock wall");
-    Object * stick = new Object('/', "stick");
     
     Map newMap;
     
     newMap.addObjectAt(stick, 5, 3);
 
     std::cout << "Stick X: " << stick->x() << " Y: " << stick->y() << std::endl;
-    newMap.addObjectAt(rock, 1, 1);
-    newMap.addObjectAt(rock, 1, 2);
-    newMap.addObjectAt(rock, 1, 3);
-    newMap.addObjectAt(rock, 1, 4);
-    newMap.addObjectAt(rock, std::tuple<int, int>(1,5));
-    newMap.addObjectAt(rock, std::tuple<int, int>(1,6));
-    newMap.addObjectAt(rock, std::tuple<int, int>(1,7));
-    newMap.addObjectAt(rock, std::tuple<int, int>(2,1));
-    newMap.addObjectAt(rock, std::tuple<int, int>(3,1));
-    newMap.addObjectAt(rock, std::tuple<int, int>(4,1));
-    newMap.addObjectAt(rock, std::tuple<int, int>(5,1));
-    newMap.addObjectAt(rock, std::tuple<int, int>(2,7));
-    newMap.addObjectAt(rock, std::tuple<int, int>(3,7));
-    newMap.addObjectAt(rock, std::tuple<int, int>(4,7));
-    newMap.addObjectAt(rock, std::tuple<int, int>(5,7));
-    newMap.addObjectAt(rock, std::tuple<int, int>(6,1));
-    newMap.addObjectAt(rock, std::tuple<int, int>(6,2));
-    newMap.addObjectAt(rock, std::tuple<int, int>(6,3));
-    newMap.addObjectAt(rock, std::tuple<int, int>(6,5));
-    newMap.addObjectAt(rock, std::tuple<int, int>(6,6));
-    newMap.addObjectAt(rock, std::tuple<int, int>(6,7));
-    newMap.addObjectAt(rock, std::tuple<int, int>(7,3));
-    newMap.addObjectAt(rock, std::tuple<int, int>(7,5));
-    newMap.addObjectAt(rock, std::tuple<int, int>(8,3));
-    newMap.addObjectAt(rock, std::tuple<int, int>(8,5));
-    newMap.addObjectAt(rock, std::tuple<int, int>(9,3));
-    newMap.addObjectAt(rock, std::tuple<int, int>(10,3));
-    newMap.addObjectAt(rock, std::tuple<int, int>(11,3));
-    newMap.addObjectAt(rock, std::tuple<int, int>(9,5));
-    newMap.addObjectAt(rock, std::tuple<int, int>(10,5));
-    newMap.addObjectAt(rock, std::tuple<int, int>(11,5));
+    newMap.addObjectAt(wall, 1, 1);
+    newMap.addObjectAt(wall, 1, 2);
+    newMap.addObjectAt(wall, 1, 3);
+    newMap.addObjectAt(wall, 1, 4);
+    newMap.addObjectAt(wall, std::tuple<int, int>(1,5));
+    newMap.addObjectAt(wall, std::tuple<int, int>(1,6));
+    newMap.addObjectAt(wall, std::tuple<int, int>(1,7));
+    newMap.addObjectAt(wall, std::tuple<int, int>(2,1));
+    newMap.addObjectAt(wall, std::tuple<int, int>(3,1));
+    newMap.addObjectAt(wall, std::tuple<int, int>(4,1));
+    newMap.addObjectAt(wall, std::tuple<int, int>(5,1));
+    newMap.addObjectAt(wall, std::tuple<int, int>(2,7));
+    newMap.addObjectAt(wall, std::tuple<int, int>(3,7));
+    newMap.addObjectAt(wall, std::tuple<int, int>(4,7));
+    newMap.addObjectAt(wall, std::tuple<int, int>(5,7));
+    newMap.addObjectAt(wall, std::tuple<int, int>(6,1));
+    newMap.addObjectAt(wall, std::tuple<int, int>(6,2));
+    newMap.addObjectAt(wall, std::tuple<int, int>(6,3));
+    newMap.addObjectAt(wall, std::tuple<int, int>(6,5));
+    newMap.addObjectAt(wall, std::tuple<int, int>(6,6));
+    newMap.addObjectAt(wall, std::tuple<int, int>(6,7));
+    newMap.addObjectAt(wall, std::tuple<int, int>(7,3));
+    newMap.addObjectAt(wall, std::tuple<int, int>(7,5));
+    newMap.addObjectAt(wall, std::tuple<int, int>(8,3));
+    newMap.addObjectAt(wall, std::tuple<int, int>(8,5));
+    newMap.addObjectAt(wall, std::tuple<int, int>(9,3));
+    newMap.addObjectAt(wall, std::tuple<int, int>(10,3));
+    newMap.addObjectAt(wall, std::tuple<int, int>(11,3));
+    newMap.addObjectAt(wall, std::tuple<int, int>(9,5));
+    newMap.addObjectAt(wall, std::tuple<int, int>(10,5));
+    newMap.addObjectAt(wall, std::tuple<int, int>(11,5));
 
     TCODConsole::initRoot(80, 50, "vigilant-waddle", false);
+    
+    return {
+        newMap, 
+        std::make_tuple(player_x, player_y)
+    };
+}
+
+/*
+ *  Load a map from a file-enclose and return its maximum x, y dimensions as 
+ *  a std::tuple, skipping empty lines or lines beginning with "//"
+ *
+ *  @param std::string
+ */
+std::tuple<int, int> getMaxDimensions(std::string filename) {
+    int max_x = 0;
+    int max_y = 0;
+    std::ifstream ifs { filename };
+    
+    while (ifs) {
+        std::string currentLine;
+        
+        std::getline(ifs, currentLine);
+        if (currentLine.length() > 0 && currentLine.substr(0, 2) != "//") {
+            if (max_x < currentLine.length()) {
+                max_x = currentLine.length();
+            }
+            max_y++;
+        }
+    }
+    
+    return std::make_tuple(max_x, max_y);
+}
+
+/*
+ *  Load a map from a file, render it to a Map, and return that Map and the 
+ *  player Coordinates
+ * 
+ *  If a player was found, the last player's coordinates are returned. If no
+ *  player was found, (0,0) are returned as the player coordinates.
+ *
+ *  @param std::string
+ */
+std::tuple<Map, Coordinates> loadMap(std::string filename) {
+    Object * wall  = new Object('#', "rock wall");
+    Object * floor = new Object('.', "wall floor");
+    Object * stick = new Object('/', "stick");
+    int player_x = 0;
+    int player_y = 0;
+    
+    Map newMap;
+    
+    std::ifstream ifs { filename };
+    
+    if (!ifs) {
+        std::cerr << "loadMap.cpp: Could not open \"" << filename << "\"!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    auto max_dimensions = getMaxDimensions(filename);
+    int max_x = std::get<0>(max_dimensions);
+    int max_y = std::get<1>(max_dimensions);
+    
+    if (max_x <= 0 || max_y <= 0) {
+        std::cerr << "loadMap.cpp: Invalid map \"" << filename << "\"!" << std::endl;
+        exit(EXIT_FAILURE);
+    }
+    
+    TCODConsole::initRoot(max_x, max_y, "vigilant-waddle", false);
+    
+    int curr_x = 0;
+    int curr_y = 0;
+        
+    while (ifs) {
+        std::string currentLine;
+        
+        std::getline(ifs, currentLine);
+        // Skip lines that are empty or begin with "//"
+        if (currentLine.substr(0, 2) == "//") {
+            continue;
+        }
+        for (auto iter = currentLine.cbegin(); iter != currentLine.cend(); ++iter) {
+            switch (*iter) {
+                case '#': // wall
+                    newMap.addObjectAt(wall, curr_x, curr_y);
+                    break;
+                case '.': // floor
+                    newMap.addObjectAt(floor, curr_x, curr_y);
+                    break;
+                case '@': // player
+                    TCODConsole::root->putChar(curr_x, curr_y, '@');
+                    player_x = curr_x;
+                    player_y = curr_y;
+                    break;
+                case '/': // stick
+                    newMap.addObjectAt(stick, curr_x, curr_y);
+                    break;
+                default:
+                    break;
+            }
+            curr_x++;
+        }
+        curr_x = 0; // reset curr_x for next row
+        if (currentLine.length() > 0) {
+            curr_y++;
+        }
+    }
+    
+    return {
+        newMap, 
+        std::make_tuple(player_x, player_y)
+    };
+}
+
+int main() {
+    Map newMap; 
+    int player_x, player_y;
+    
+    //auto ret = renderTestMap();
+    auto ret = loadMap("./maps/test.txt");
+    
+    newMap = std::get<0>(ret);
+    player_x = std::get<0>(std::get<1>(ret));
+    player_y = std::get<1>(std::get<1>(ret));
 
     while ( !TCODConsole::isWindowClosed() ) {   
         // Handle key input
         TCOD_key_t key;
         
         TCODSystem::checkForEvent(TCOD_EVENT_KEY_PRESS, &key, NULL);
-        switch(key.vk) {
+        switch (key.vk) {
             case TCODK_UP:
             case TCODK_KP8:
                 player_y--; 
