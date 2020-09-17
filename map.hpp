287f8041e8225884d613a11760101d918fbe1b1d
Krotera <01101011@tuta.io>
2020-09-16 21:54:56 -0400

    Actually not successfully last commit cuz I forgot to refactor this one method in Map but now I did that and it IS successful again, hell yeah

--- map.hpp
+++ map.hpp
@@ -14,7 +14,7 @@ class Map {
     public:
         bool isObjectAt(std::tuple<int, int> coords);
         bool isObjectAt(int x, int y);
-        Object fetchObject(std::tuple<int, int> coords);
+        Object fetchObjectAt(std::tuple<int, int> coords);
         Object fetchObjectAt(int x, int y);
         bool addObjectAt(Object * obj, std::tuple<int, int> coords);
         bool addObjectAt(Object * obj, int x, int y);
